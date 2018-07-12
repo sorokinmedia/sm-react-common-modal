e63ab734f826b94c4b3cc7b1be767c1a014a457e
magarampage <magarampage@gmail.com>
2018-07-12 19:54:23 +0300

    examples

--- examples/index.js
+++ examples/index.js
@@ -1,65 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { ModalForm } from '../lib';
 
-class App extends React.Component {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		}
+	}
+
 	handleModal = () => {
-		this.setState(prevState => ({
-			modal: !prevState.modal
+		this.setState(({ modal }) => ({
+			modal: !modal
 		}))
 	}
 
 	render() {
-
-		return (
-			<div>
-				<button openModal={this.handleModal}>open modal</button>
-				{/*<ModalForm*/}
-				{/*modalIsOpen={this.state.modal}*/}
-				{/*title="Title"*/}
-				{/*closeModal={this.handleModal}*/}
-				{/*/>*/}
-			</div>)
-	}
-}
-
-App.propTypes = {}
-App.defaultProps = {}
-
-render(<App />, document.getElementById('root'));
-
-
-/*
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-// import { ModalForm } from '../lib';
-
-class App extends Component() {
-	//
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		modal: false
-	// 	}
-	// }
-	//
-	// handleModal = () => {
-	// 	this.setState(prevState => ({
-	// 		modal: !prevState.modal
-	// 	}))
-	//
-	// }
-
-	render() {
-		console.log('sdf');
 		return (
 			<div>
 				<button onClick={this.handleModal}>open modal</button>
-				{/!*<ModalForm*!/}
-				{/!*modalIsOpen={this.state.modal}*!/}
-				{/!*title="Title"*!/}
-				{/!*closeModal={this.handleModal}*!/}
-				{/!*!/>*!/}
+				<ModalForm
+					modalIsOpen={this.state.modal}
+					title="Title"
+					closeModal={this.handleModal}
+				/>
 			</div>)
 	}
 }
@@ -68,4 +33,3 @@ App.propTypes = {}
 App.defaultProps = {}
 
 render(<App />, document.getElementById('root'));
-*/
