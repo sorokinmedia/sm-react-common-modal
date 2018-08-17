27dcbff04b36466f38c390eba5d24581be59f94a
magarampage <magarampage@gmail.com>
2018-08-17 16:38:56 +0300

    [fix] modal style issues

--- src/Modal/index.test.js
+++ src/Modal/index.test.js
@@ -1,9 +1,9 @@
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import React from 'react';
-import Modal from './index';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+import Modal from './index'
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 function setup(customProps, lifeCycle = false) {
 	const props = { ...customProps }
@@ -16,25 +16,57 @@ function setup(customProps, lifeCycle = false) {
 
 
 describe('Modal', () => {
-	const click = jest.fn();
+	const click = jest.fn()
 	const { container } = setup({ isOpen: true, onRequestHide: click })
 
 	it('should render the component', () => {
-		expect(container.exists()).toBe(true);
-	});
+		expect(container.exists()).toBe(true)
+	})
 
 	it('should the component has div with the required class', () => {
 		expect(container.find('.modal-backdrop')).toBeTruthy()
-	});
+	})
 
 	it('should backdrop clicked once', () => {
-		const instance = container.instance();
-		const spy = jest
-			.spyOn(instance, 'handleAreaClick')
-			.mockImplementation(() => true)
+		const instance = container.instance()
+		jest.spyOn(instance, 'handleAreaClick')
 		container.instance().forceUpdate()
 		container.update()
-		container.find('.modal-backdrop').simulate('click');
-		expect(spy).toHaveBeenCalled()
-	});
+		container.find('.modal-backdrop').simulate(
+			'click',
+			{
+				target: {
+					classList: {
+						contains:
+							jest.fn(() => true)
+					}
+				}
+			}
+		)
+		expect(click).toHaveBeenCalled()
+		click.mockReset()
+	})
+
+	it('should backdrop clicked return none', () => {
+		const instance = container.instance()
+		jest.spyOn(instance, 'handleAreaClick')
+		container.instance().forceUpdate()
+		container.update()
+		container.find('.modal-backdrop').simulate(
+			'click',
+			{
+				target: {
+					classList: {
+						contains:
+							jest.fn(() => false)
+					}
+				}
+			}
+		)
+		expect(click).not.toHaveBeenCalled()
+	})
+	it('should return null', () => {
+		const wrapper = shallow(<Modal isOpen={false} />)
+		expect(wrapper.type()).toEqual(null)
+	})
 })
